4f298222db635fb6873bd69352c4f99490052435
omq78 <omar.qab@gmail.com>
2019-07-26 02:13:48 +0300

    Initial Commit

---
+++ EdgePadingViewsLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EdgePadingViewsLayout
+//
+//  Created by Omar Alqabbani on 7/26/19.
+//  Copyright © 2019 OmarALqabbani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
